b7638e43d96033bdf3bd4e56ce0f46a4e0dcd809
Greg Maza <mazag52@gmail.com>
2013-03-06 07:51:47 -0600

    Rails Original Files

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Kid3000::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kid3000::Application.config.secret_token = '315d28e61d1dc1164b8679bef8a4adc5613d1cc5babddc6d4f3e22de8d7e0d40a2a644b3a38300c95819a8ba522f850f2c8d1b7db6d617f6e4ad76ba7ebe3b74'
